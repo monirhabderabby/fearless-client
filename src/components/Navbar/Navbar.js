5a3e64c1bd836eb1a5b99c96dab2e059b110aff1
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-27 22:45:41 +0600

    authentication completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,21 @@
 
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/Firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+    if(user){
+        console.log(user);
+    }
+    if(loading){
+        return
+    }
+    const handleSignOut = () =>{
+        signOut(auth)
+    }
     
     return (
         <nav className='nav'>
@@ -12,12 +25,12 @@ const Navbar = () => {
             <div className='nav-item'>
                 <Link to="/" className='menu'>Home</Link>
                 <Link to="/" className='menu'>Cart</Link>
-                {/* {
-                    isUser ?
-                    <Link to="/login" className='menu'>Login</Link>
+                {
+                    user ?
+                    <Link to="/login" className='menu' onClick={handleSignOut}>SignOut</Link>
                     :
-                    <Link to="/login" className='menu'>SignOut</Link>
-                } */}
+                    <Link to="/login" className='menu'>Login</Link>
+                }
             </div>
         </nav>
     );
